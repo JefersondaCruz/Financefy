ce926757cd1ba2a08627b75357ff72a9a7b3b925
JefersondaCruz <jefersondacruz40@gmail.com>
2025-10-11 20:16:28 -0300

    feat: initial project setup with Docker, architecture, migrations and models

---
+++ application/app/Models/Transaction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'description',
+        'amount',
+        'transaction_date',
+        'payment_method',
+        'is_recurring',
+        'recurrence_type'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
